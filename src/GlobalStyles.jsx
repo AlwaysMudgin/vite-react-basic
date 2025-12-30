a000bb1dc0106c280e0d46944661e0e3327ae14a
AlwaysMudgin <193750882+AlwaysMudgin@users.noreply.github.com>
2025-12-29 21:50:07 -0500

    - Removed placeholder content and CSS files - Added styled components for CSS - Added CSS reset - Added lucide react for Icons - Added react router for Routing - Added vitest, jsdom, react testing library and user events for Testing

---
+++ src/GlobalStyles.jsx
@@ -0,0 +1,53 @@
+import { createGlobalStyle } from 'styled-components';
+import Sora from '../../assets/fonts/Sora/Sora-VariableFont_wght.ttf';
+
+// Base is JWC reset
+
+const GlobalStyles = createGlobalStyle`
+*, *::before, *::after {
+  box-sizing: border-box;
+}
+
+*:not(dialog) {
+  margin: 0;
+}
+
+@media (prefers-reduced-motion: no-preference) {
+  html {
+    interpolate-size: allow-keywords;
+  }
+}
+
+body {
+  line-height: 1.5;
+  -webkit-font-smoothing: antialiased;
+}
+
+img, picture, video, canvas, svg {
+  display: block;
+  max-width: 100%;
+}
+
+input, button, textarea, select {
+  font: inherit;
+}
+
+p, h1, h2, h3, h4, h5, h6 {
+  overflow-wrap: break-word;
+}
+
+p {
+  text-wrap: pretty;
+}
+
+h1, h2, h3, h4, h5, h6 {
+  text-wrap: balance;
+}
+
+#root, #__next {
+  isolation: isolate;
+}
+
+`;
+
+export default GlobalStyles;
